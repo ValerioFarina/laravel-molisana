3c68c7eb04925b89aa296edc236ba3a64bc9b1bd
Valerio Farina <valerio.farina.1994@gmail.com>
2020-12-22 14:37:16 +0100

    Aggiunte le view "home" e "products"

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return 'hello';
+    return view('home');
 });
+
+Route::get('/prodotti', function () {
+    return view('products');
+})->name('products');
