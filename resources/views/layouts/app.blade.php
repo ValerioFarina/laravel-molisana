83d170dcededa3d7b3d2678b0af5ca1b3d8e813a
Valerio Farina <valerio.farina.1994@gmail.com>
2020-12-22 14:49:30 +0100

    Aggiunte immagini alla cartella public

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,7 +2,7 @@
 <html lang="en" dir="ltr">
     <head>
         <meta charset="utf-8">
-        <title>@yield('page-title') - La Molisana</title>
+        <title>La Molisana - @yield('page-title')</title>
     </head>
     <body>
         <header>
