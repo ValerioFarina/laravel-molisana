9c7b434f417fb21defb4cf1625690edca42c44dd
Valerio Farina <valerio.farina.1994@gmail.com>
2020-12-23 16:44:28 +0100

    Inseriti titolo e descrizione nella view "product-details"

--- resources/views/product-details.blade.php
+++ resources/views/product-details.blade.php
@@ -3,8 +3,22 @@
 @section('page-title', 'Dettagli prodotto')
 
 @section('content')
-    <div class="product-images">
-        <img src="{{ $product["src-h"] }}" alt="{{ $product["titolo"] }}">
-        <img src="{{ $product["src-p"] }}" alt="pacchetto pasta">
+    <div class="product-info">
+        <div class="container">
+            <h1>
+                {{ $product["titolo"] }}
+            </h1>
+        </div>
+
+        <div class="product-images">
+            <img src="{{ $product["src-h"] }}" alt="{{ $product["titolo"] }}">
+            <img src="{{ $product["src-p"] }}" alt="pacchetto pasta">
+        </div>
+
+        <div class="container">
+            <p>
+                {!! $product["descrizione"] !!}
+            </p>
+        </div>
     </div>
 @endsection
