a5a63eb7a8aa0edd8ae2c1de9e2ac8552b00723f
Valerio Farina <valerio.farina.1994@gmail.com>
2020-12-28 15:01:52 +0100

    Aggiunta immagine del prodotto precedente (successivo) quando si va in hover sulla freccia di sinistra (destra)

--- app/helpers.php
+++ app/helpers.php
@@ -4,10 +4,18 @@ function is_current_page ($page) {
     return Request::route()->getName() == $page;
 }
 
-function prev_product ($pasta_types, $product_id) {
+function prev_product_route ($pasta_types, $product_id) {
     return array_key_exists($product_id - 1, $pasta_types) ? route('product-details', ['id' => $product_id - 1]) : route('product-details', ['id' => count($pasta_types) - 1]);
 }
 
-function next_product ($pasta_types, $product_id) {
+function next_product_route ($pasta_types, $product_id) {
     return array_key_exists($product_id + 1, $pasta_types) ? route('product-details', ['id' => $product_id + 1]) : route('product-details', ['id' => 0]);
+}
+
+function prev_product_img ($pasta_types, $product_id) {
+    return array_key_exists($product_id - 1, $pasta_types) ? $pasta_types[$product_id - 1]["src"] : $pasta_types[count($pasta_types) - 1]["src"];
+}
+
+function next_product_img ($pasta_types, $product_id) {
+    return array_key_exists($product_id + 1, $pasta_types) ? $pasta_types[$product_id + 1]["src"] : $pasta_types[0]["src"];
 }
--- resources/views/product-details.blade.php
+++ resources/views/product-details.blade.php
@@ -3,13 +3,23 @@
 @section('page-title', 'Dettagli prodotto')
 
 @section('content')
-    <a href="{{ prev_product($pasta_types, $product_id) }}" class="arrow left">
-        <i class="fas fa-long-arrow-alt-left"></i>
+    <a href="{{ prev_product_route($pasta_types, $product_id) }}" class="prev-product">
+        <div class="product-image">
+            <img src="{{ prev_product_img($pasta_types, $product_id) }}" alt="">
+        </div>
+        <div class="arrow">
+            <i class="fas fa-long-arrow-alt-left"></i>
+        </div>
     </a>
-    <a href="{{ next_product($pasta_types, $product_id) }}" class="arrow right">
-        <i class="fas fa-long-arrow-alt-right"></i>
+    <a href="{{ next_product_route($pasta_types, $product_id) }}" class="next-product">
+        <div class="arrow">
+            <i class="fas fa-long-arrow-alt-right"></i>
+        </div>
+        <div class="product-image">
+            <img src="{{ next_product_img($pasta_types, $product_id) }}" alt="">
+        </div>
     </a>
-    
+
     <div class="product-info">
         <div class="container">
             <h1>
